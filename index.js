802b0737ed8ffef82f611e15f11b6ae64044f6e4
Alamgir Ahamed Saif <36258593+alamgir72435@users.noreply.github.com>
2021-03-27 22:04:11 +0600

    ok

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello world</h1>");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`server Running on port ${PORT}`));
